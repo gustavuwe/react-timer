e0abb4edb562d60ce66b00c30333a5d8beaed3b3
Gustav Uwe <gustavuwe.123@gmail.com>
2024-11-28 19:14:20 -0300

    feat: add thema configuration with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="success" />
+        <Button variant="danger" />
+      </ThemeProvider>
     </>
   );
 }
