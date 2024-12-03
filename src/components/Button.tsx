9b83ce53d676bd63ab1746ca7554b538346b4528
Gustav Uwe <gustavuwe.123@gmail.com>
2024-12-03 18:43:25 -0300

    chore: add eslint configuration

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,6 @@ interface ButtonProps {
   variant?: ButtonVariant;
 }
 
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+export function Button({ variant = "primary" }: ButtonProps) {
+  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
 }
