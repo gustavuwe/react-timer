8f71c583da562c97c845f50f07d6f9274a1aa112
Gustav Uwe <gustavuwe.123@gmail.com>
2024-12-03 17:43:51 -0300

    chore: add fonts, colors and theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Button } from "./components/Button";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/globals";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
