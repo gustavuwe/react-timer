82415d19f7f1c5e473db3fe7841cf31b585670ee
Gustav Uwe <gustavuwe.123@gmail.com>
2024-12-31 12:26:59 -0300

    add zod validation into form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -12,14 +14,21 @@ import {
 } from "./styles";
 import { useState } from "react";
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'), // if the users doesnt pass a value, then says that
+  minutesAmount: zod.number().min(5, "O ciclo precisa ser de no mínimo 5 minutos.").max(60, "O ciclo precisa ser de no máximo 60 minutos."),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema)
+  })
 
   function handleCreateNewCycle(data: any) {
 
   }
 
-  const task = watch('task')
+  const task = watch('task') // -> controlled component
   const isSubmitDisabled = !task
  
   return (
